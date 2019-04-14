26b6e8b513d4722ca072c89080e3dfe1bcdaf018
Valquiria Quirino <vfquirin@gmail.com>
2019-04-14 15:33:45 -0500

    unconf

---
+++ tests/testthat/test-rbind.R
@@ -0,0 +1,14 @@
+context("test-rbind")
+
+test_that("multiplication works", {
+
+
+
+
+    x <- factor(c("a", "b"))
+    y <- factor(c("c", "d"))
+    z <- factor(c("a", "b", "c", "d"))
+
+
+  expect_equal(2 * 2, 4)
+})
